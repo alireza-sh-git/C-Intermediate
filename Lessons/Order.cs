7beb9001374d73c062e411665e515ea0e41aaecc
Alireza Sheikhi <Sheikhi.L.Alireza@gmail.com>
2024-11-01 09:55:06 +0330

    Created the fields demo

---
+++ Lessons/Order.cs
@@ -0,0 +1,7 @@
+namespace Lessons
+{
+    class Order
+    {
+
+    }
+}
---
+++ Lessons/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lessons;
+
+
+
+
+static void FieldsDemo()
+{
+    var customer = new Customer(100, "John");
+    customer.Orders.Add(new Order());
+    customer.Orders.Add(new Order());
+    System.Console.WriteLine(customer.Orders.Count);
+    customer.Promote();
+    System.Console.WriteLine(customer.Orders.Count);
+}
